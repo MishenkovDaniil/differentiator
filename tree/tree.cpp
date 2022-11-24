27fe8f65cfd19489b7985f5ee8bb6435ddbdf924
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-25 00:32:14 +0300

    programm for creating, changing, adding, dumping and destructoring differentiator tree

---
+++ tree/tree.cpp
@@ -0,0 +1,325 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tree.h"
+
+Node *tree_create_node (Type type, const char *value, Node *left, Node *right)
+{
+    Node *node = (Node *)calloc (1, sizeof (Node));
+    assert (node);
+
+    if (node == nullptr)
+    {
+        return printf ("calloc failed"), nullptr;
+    }
+
+    switch (type)
+    {
+        case TYPE_DEFAULT: break;
+        case TYPE_OP:
+        {
+            node->type = TYPE_OP;
+
+            if (strcasecmp ("OP_DEFAULT", value) == 0)
+            {
+                node->value.op_val = OP_DEFAULT;
+            }
+            else if (strcasecmp ("OP_ADD", value) == 0)
+            {
+                node->value.op_val = OP_ADD;
+            }
+            else if (strcasecmp ("OP_SUB", value) == 0)
+            {
+                node->value.op_val = OP_SUB;
+            }
+            else if (strcasecmp ("OP_MUL", value) == 0)
+            {
+                node->value.op_val = OP_MUL;
+            }
+            else if (strcasecmp ("OP_DIV", value) == 0)
+            {
+                node->value.op_val = OP_DIV;
+            }
+            else if (strcasecmp ("OP_SIN", value) == 0)
+            {
+                node->value.op_val = OP_SIN;
+            }
+            else if (strcasecmp ("OP_COS", value) == 0)
+            {
+                node->value.op_val = OP_COS;
+            }
+            else
+            {
+                printf ("Error: wrong value type %d", type);
+                return nullptr;
+            }
+
+            break;
+        }
+        case TYPE_VAR:
+        {
+            node->type = TYPE_VAR;
+
+            node->value.var = (char *)calloc (strlen (value) + 1, sizeof (char));
+            strcpy (node->value.var, value);
+
+            break;
+        }
+        case TYPE_NUM:
+        {
+            node->type = TYPE_NUM;
+
+            node->value.dbl_val = atof (value);
+
+            break;
+        }
+        default:
+        {
+            printf ("Error: wrong value type %d", type);
+            return nullptr;
+        }
+    }
+
+    node->left = left;
+    node->right = right;
+
+    return node;
+}
+
+void tree_ctor (Tree *tree, unsigned int *err)
+{
+    assert (tree);
+
+    if (tree == nullptr)
+    {
+        printf ("wrong tree pointer");
+        return;
+    }
+    tree->root = tree_create_node (TYPE_OP, "OP_MUL");
+    assert (tree->root);
+}
+
+void add_nodes (Tree *tree, Node *parent_node, Type left_type, Type right_type, char *left_value, char *right_value, unsigned int *err)
+{
+    assert (tree);
+    assert (parent_node);
+
+    if (parent_node == nullptr)
+    {
+        printf ("TREE_ERROR: incorrect pointer to parent node.\n");
+
+        return;
+    }
+    if (tree_check (tree, err))
+    {
+        return;
+    }
+
+    Node *new_node_left = tree_create_node (left_type, left_value);
+    Node *new_node_right = tree_create_node (right_type, right_value);
+
+    if (new_node_left == nullptr || new_node_right == nullptr)
+    {
+        return;
+    }
+
+    parent_node->left = new_node_left;
+    parent_node->right = new_node_right;
+
+    new_node_left->parent = parent_node;
+    new_node_right->parent = parent_node;
+
+    tree_check (tree, err);
+}
+
+void tree_dtor (Tree *tree, unsigned int *err)
+{
+    assert (tree && tree->root);
+
+    if (tree && tree->root)
+    {
+        tree_free (tree->root);
+    }
+    else
+    {
+        printf ("nothing to dtor");
+    }
+}
+
+void tree_free (Node *node)
+{
+    if (node->left)
+    {
+        //node->left->value = nullptr;
+        tree_free(node->left);
+    }
+    if (node->right)
+    {
+        //node->right->data = nullptr;
+        tree_free(node->right);
+    }
+
+    if (node->type == TYPE_VAR)
+    {
+        if (node->value.var)
+        {
+            free (node->value.var);
+        }
+    }
+    free (node);
+
+    node = nullptr;
+}
+
+
+unsigned int tree_check (Tree *tree, unsigned int *err)
+{
+    //if (*err)
+    {
+        tree_dump (tree, err);
+
+        //return *err;
+    }
+
+    return 0;
+}
+
+void tree_dump (Tree *tree, unsigned int *err)
+{
+    tree_graph (tree);
+}
+
+void tree_graph (Tree *tree)
+{
+    static int PNG_FILE_NUMBER = 0;
+
+    FILE *tgraph_file = fopen ("tree_graph", "w");
+    FILE *tree_log  = nullptr;
+
+    if (!(PNG_FILE_NUMBER))
+    {
+        tree_log  = fopen ("tree_dump.html", "w");
+    }
+    else
+    {
+        tree_log  = fopen ("tree_dump.html", "a");
+    }
+
+    fprintf (tgraph_file, "digraph\n{\n\t");
+
+    make_graph_nodes (tree->root, tgraph_file);
+
+    fprintf (tgraph_file, "}");
+    fclose (tgraph_file);
+
+    const int CMD_LEN = 100;
+    char cmd[CMD_LEN] = "";
+
+    sprintf (cmd, "Dot tree_graph -T png -o tree_dots/tree_dot%d.png", PNG_FILE_NUMBER);
+
+    printf ("%s", cmd);
+
+    system ((const char *)cmd);
+
+    fprintf (tree_log, "<pre>\n<img src = tree_dots/tree_dot%d.png>\n", PNG_FILE_NUMBER++);
+
+    fclose (tree_log);
+}
+
+int make_graph_nodes (Node *node, FILE *tgraph_file)
+{
+    static int graph_num = 0;
+
+    int node_num = graph_num;
+
+    if (node == nullptr)
+    {
+        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"orange\", label = \"NULL\"];\n\t", graph_num++);
+    }
+    else
+    {
+        switch (node->type)
+        {
+            case TYPE_DEFAULT:
+            {
+                fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"orange\", label = \"DEFAULT\"];\n\t", graph_num++);
+                break;
+            }
+            case TYPE_OP:
+            {
+                switch (node->value.op_val)
+                {
+                    case OP_DEFAULT:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"orange\", label = \"{node %p | {OP | DEFAULT} | {L %p | R %p}} \"];\n\t", graph_num++, node, node->left, node->right);
+                        break;
+                    }
+                    case OP_ADD:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {OP | %c} | {L %p | R %p}} \"];\n\t", graph_num++, node, '+', node->left, node->right);
+                        break;
+                    }
+                    case OP_SUB:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {OP | %c} | {L %p | R %p}} \"];\n\t", graph_num++, node, '-', node->left, node->right);
+                        break;
+                    }
+                    case OP_MUL:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {OP | %c} | {L %p | R %p}} \"];\n\t", graph_num++, node, '*', node->left, node->right);
+                        break;
+                    }
+                    case OP_DIV:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {OP | %c} | {L %p | R %p}} \"];\n\t", graph_num++, node, '/', node->left, node->right);
+                        break;
+                    }
+                    case OP_SIN:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {OP | %s} | {L %p | R %p}} \"];\n\t", graph_num++, node, "sin", node->left, node->right);
+                        break;
+                    }
+                    case OP_COS:
+                    {
+                        fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {OP | %s} | {L %p | R %p}} \"];\n\t", graph_num++, node, "cos", node->left, node->right);
+                        break;
+                    }
+                    default:
+                    {
+                        printf ("Error: wrong node op type in %s", __PRETTY_FUNCTION__);
+                        break;
+                    }
+                }
+                break;
+            }
+            case TYPE_NUM:
+            {
+                fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {NUM | %.2lf} | {L %p | R %p}} \"];\n\t", graph_num++, node, node->value.dbl_val, node->left, node->right);
+                break;
+            }
+            case TYPE_VAR:
+            {
+                fprintf (tgraph_file, "node_%d [shape = record, style = \"filled\", fillcolor = \"lightblue\", label = \"{node %p | {VAR | %s} | {L %p | R %p}} \"];\n\t", graph_num++, node, node->value.var, node->left, node->right);
+                break;
+            }
+            default:
+            {
+                printf ("Error: wrong node type in %s", __PRETTY_FUNCTION__);
+                break;
+            }
+        }
+    }
+
+    if (node->left != nullptr && node->right != nullptr)
+    {
+        int left = make_graph_nodes (node->left, tgraph_file);
+        int right = make_graph_nodes (node->right, tgraph_file);
+
+        fprintf (tgraph_file, "node_%d->node_%d;\n\t", node_num, left);
+        fprintf (tgraph_file, "node_%d->node_%d;\n\t", node_num, right);
+    }
+
+    return node_num;
+}
---
+++ tree/tree.h
@@ -0,0 +1,61 @@
+#ifndef DEFINITIATOR_TREE_H
+#define DEFINITIATOR_TREE_H
+
+enum Type
+{
+    TYPE_DEFAULT = 0,
+    TYPE_OP  = 1,
+    TYPE_VAR = 2,
+    TYPE_NUM = 3,
+};
+
+enum Op_val
+{
+    OP_DEFAULT = 0,
+    OP_ADD = 1,
+    OP_SUB = 2,
+    OP_MUL = 3,
+    OP_DIV = 4,
+    OP_SIN = 5,
+    OP_COS = 6,
+};
+
+union Value
+{
+    double dbl_val;
+    Op_val op_val;
+    char *var;
+};
+
+struct Node
+{
+    Type type = TYPE_DEFAULT;
+
+    Value value = {};
+
+    Node *left = nullptr;
+    Node *right = nullptr;
+    Node *parent = nullptr;
+};
+
+struct Tree
+{
+    Node *root = nullptr;//add debug info
+};
+
+static unsigned int DEF_ERRNO = 0;
+
+Node *tree_create_node (Type type, const char *value, Node *left = nullptr, Node *right = nullptr);
+
+void tree_ctor (Tree *tree, unsigned int *err = &DEF_ERRNO);
+void add_nodes (Tree *tree, Node *parent_node, Type left_type, Type right_type,
+               char *left_value, char *right_value, unsigned int *err = &DEF_ERRNO);
+void tree_dtor (Tree *tree, unsigned int *err = &DEF_ERRNO);
+void tree_free (Node *node);
+
+unsigned int tree_check (Tree *tree, unsigned int *err);
+void tree_dump (Tree *tree, unsigned int *err);
+void tree_graph (Tree *tree);
+int make_graph_nodes (Node *node, FILE *tgraph_file);
+
+#endif /* DEFINITIATOR_TREE_H */
