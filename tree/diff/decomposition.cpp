d5ad1ddfbf2f617bb84cd607eeb6b6ea990346c5
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-29 14:56:21 +0300

    made function decomposition by Taylor series

---
+++ tree/diff/decomposition.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "../tree.h"
+#include "tree_diff.h"
+#include "func_value.h"
+#include "decomposition.h"
+
+#define create_num(num)                 tree_create_num (num)
+#define create_var(var)                 tree_create_node (TYPE_VAR, #var)
+#define ADD(left_node, right_node)      tree_create_node (TYPE_OP, "OP_ADD", left_node, right_node)
+#define SUB(left_node, right_node)      tree_create_node (TYPE_OP, "OP_SUB", left_node, right_node)
+#define MUL(left_node, right_node)      tree_create_node (TYPE_OP, "OP_MUL", left_node, right_node)
+#define DEG(left_node, right_node)      tree_create_node (TYPE_OP, "OP_DEG", left_node, right_node)
+
+Node *decompose (Tree *taylor_tree, const Node *node, int decompose_deg, double point)
+{
+    int diff_order = 0;
+
+    double *values = (double *)calloc (decompose_deg + 1, sizeof (double));
+
+    values[0] = node_point_value (node, point);
+
+    Node *diff_tree_root = tree_diff (node);
+    Node *taylor = create_num (values[0]);
+
+    int max_diff_order = decompose_deg;
+
+    for (int i = 1; i <= max_diff_order; i++)
+    {
+        values[i] = node_point_value (diff_tree_root, point);
+
+        if (i == 1)
+        {
+            taylor = ADD (taylor, MUL(create_num (values[1]), SUB (create_var (x), create_num (point))));
+        }
+        else
+        {
+            taylor = ADD (taylor, MUL (create_num (values[i]), DEG (SUB (create_var (x), create_num (point)), create_num (i))));
+        }
+
+        Node *temp_node = diff_tree_root;
+
+        if (i < decompose_deg)
+        {
+            diff_tree_root = tree_diff (diff_tree_root);
+        }
+
+        tree_free (temp_node);
+    }
+
+    taylor_tree->root = taylor;
+
+    free (values);
+
+    return taylor;
+}
+
+#undef create_num
+#undef create_var
+#undef ADD
+#undef SUB
+#undef MUL
+#undef DEG
---
+++ tree/diff/decomposition.h
@@ -0,0 +1,8 @@
+#ifndef DECOMPOSITION_H
+#define DECOMPOSITION_H
+
+#include "../tree.h"
+
+Node *decompose (Tree *taylor_tree, const Node *node, int decompose_deg, double point);
+
+#endif /* DECOMPOSITION_H */
