4267d7c11079dca5e34c55ac154b09f4305fc2d2
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-12-01 20:41:32 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -1,53 +1,68 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "tree/tree.h"
 #include "tree/io/tree_input.h"
 #include "tree/io/tree_output.h"
 #include "tree/diff/tree_diff.h"
 #include "tree/tree_convolution.h"
-#include "tree/func_value.h"
+#include "tree/diff/func_value.h"
+#include "tree/diff/decomposition.h"
+
+#define tex_print(...) fprintf (tex_file, __VA_ARGS__)
 
 int main ()
 {
-    const char *input_file_name = "def_input.txt";
-    const char *tex_file_name = "AAAAAA_diffurs.txt";
+    const char *input_file_name   = "def_input.txt";
+    const char *tex_file_name     = "AAAAAA_diffurs.txt";
+    const char *graphic_file_name = "graphic.txt";
+    const char *func_name         = "f";
 
     unsigned int errr = 0;
     unsigned int *err = &errr;
 
-    FILE *input_file = fopen (input_file_name, "r");
-    FILE *tex_file = fopen (tex_file_name, "w");
+    FILE *input_file   = fopen (input_file_name, "r");
+    FILE *tex_file     = fopen (tex_file_name, "w");
+    FILE *graphic_file = fopen (graphic_file_name, "w");
     assert (input_file);
     assert (tex_file);
+    assert (graphic_file);
 
-    Tree tree = {};
-    tree_fill (&tree, input_file, input_file_name);
-
-    Node *func_name = tree_create_node (TYPE_VAR, "y");
+    Tree func = {};
+    tree_fill (&func, input_file, input_file_name);
 
     print_tex_header (tex_file);
+    new_section (tex_file);
 
-    fprintf (tex_file, "\\section{Глава I}\nФункция такова\\newline\n");
-    tree_tex_print (func_name, tree.root, tex_file);
-    fprintf(tex_file, "В упрощенном виде это будет выглядеть так:\\newline\n");
+//printing function
 
-    tree_check (&tree, err);
-    tree_convolution (tree.root);
-    tree_check (&tree, err);
-    tree_tex_print (func_name, tree.root, tex_file);
+    print_original_function (func_name, &func, tex_file);
+
+    tex_print ("Упростим функцию, чтобы с ней было более удобно работать.\\newline\n"
+               "Получим следующий результат:\\newline\\newline\n");
+    tree_convolution (func.root);
+
+    print_function (func_name, &func, tex_file);
+
+//printing and finding func value in point
+    double point = 0;
 
     printf ("Enter point a to find f(a):");
-    double point = 0;
     scanf ("%lf", &point);
 
-    printf ("\nVALUE IN POINT %lf IS %lf\n", point, func_point_value (&tree, point));
-    printf ("Enter diff order to find: ");
+    double func_value = func_point_value (&func, point);
+
+    print_point_value (func_name, point, func_value, tex_file);
+
+//printing and finding func diff
     int diff_order = 0;
+
+    printf ("Enter diff order to find: ");
     scanf ("%d", &diff_order);
 
-    fprintf (tex_file, "Найдем производную %d порядка этой функции методом Шварцвальда III\\newline\n", diff_order);
+    tex_print ("Найдем производную %d порядка этой функции методом Шварцвальда III\\newline\n", diff_order);
 
     if (diff_order <= 0)
     {
@@ -57,22 +72,40 @@ int main ()
 
     Tree def_tree = {};
 
-    def_tree.root = find_diff (tree.root, tex_file, diff_order);
+    def_tree.root = find_diff (func.root, tex_file, diff_order);
     tree_check (&def_tree, err);
-    fprintf (tex_file, "Для простоты запишем этот результат так:\\newline\n");
+    tex_print ("Для простоты запишем этот результат так:\\newline\n");
     tree_convolution (def_tree.root);
-    tree_check (&def_tree, err);
-    printf ("[%d]\n", __LINE__);
-    tree_tex_print (func_name, def_tree.root, tex_file, true, diff_order);
 
+    print_diff_func (func_name, &def_tree, tex_file, diff_order);
+
+//decomposing tree by Taylor and printing decomposition
+    int decompose_order = 0;
+    double decompose_point = 0;
+
+    printf ("Enter decomposition order: ");
+    scanf ("%d", &decompose_order);
+    printf ("And enter point of decomposition: ");
+    scanf ("%lf", &decompose_point);
+
+    Tree decompose_tree = {};
+    decompose (&decompose_tree, func.root, decompose_order, decompose_point);
+
+    print_decompose_tree (func_name, &decompose_tree, decompose_point, decompose_order, tex_file);
+
+    tex_print_graphic (graphic_file, graphic_file_name, input_file_name, &func, tex_file);
+
+//generating tex ends
     print_tex_ending (tex_file);
 
-
+//closing and freeing
     fclose (tex_file);
     fclose (input_file);
+    fclose (graphic_file);
 
+    tree_dtor (&decompose_tree);
     tree_dtor (&def_tree);
-    tree_dtor (&tree);
-
-    free (func_name);
+    tree_dtor (&func);
 }
+
+#undef tex_print
