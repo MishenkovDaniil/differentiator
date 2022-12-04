591c359ba1f1e71c4c9851629205aac062e1fd8c
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-12-04 21:30:54 +0300

    add file for defining similar code for different operations

---
+++ operations.h
@@ -0,0 +1,9 @@
+// DEF_OP (name, num, sign, sign len)
+
+DEF_OP (ADD, 1, +, 1)
+DEF_OP (SUB, 2, -, 1)
+DEF_OP (MUL, 3, *, 1)
+DEF_OP (DIV, 4, /, 1)
+DEF_OP (DEG, 5, ^, 1)
+DEF_OP (SIN, 6, sin, 3)
+DEF_OP (COS, 7, cos, 3)
