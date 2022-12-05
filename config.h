fc9e16071cd4cce9032bd5332691d3956d6aafe9
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-12-05 23:53:17 +0300

    add strcut config for input data

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+struct Config {
+    double point = 0;
+    int diff_order = 0;
+    int decompose_order = 0;
+    double decompose_point = 0;
+};
+
+#endif /* CONFIG_H */
