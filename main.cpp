7cf0ccaae6c7146714790ebd007c45334b03c395
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-12-13 06:02:31 +0300

    add creating pdf file

--- main.cpp
+++ main.cpp
@@ -6,11 +6,8 @@
 #include "tree/tree.h"
 #include "tree/io/tree_input.h"
 #include "tree/io/tree_output.h"
-#include "tree/diff/tree_diff.h"
-#include "tree/tree_convolution.h"
-#include "tree/diff/func_value.h"
-#include "tree/diff/decomposition.h"
 #include "config.h"
+#include "string.h"
 
 // diff --input equation -o book.tex --taylor 4 3.21 --plot graph.png --eval 6.2342341235
 // 1. Все опции должны быть как long так и short: --taylor -t
@@ -33,15 +30,17 @@
 int main (int argc, const char **argv)
 {
     const char *input_file_name   = "def_input.txt";
-    const char *tex_file_name     = "AAAAAA_diffurs.txt";
+    const char *tex_file_name     = "AAAAAA_diffurs.tex";
     const char *graphic_file_name = "graphic.txt";
     const char *func_name         = "f";
 
-    unsigned int errr = 0;
-    unsigned int *err = &errr;
+    char *tex_to_pdf_cmd = (char *)calloc (strlen ("pdflatex ") + strlen (tex_file_name) + 1, sizeof (char));
+    strcat (tex_to_pdf_cmd, "pdflatex ");
+    strcat (tex_to_pdf_cmd, tex_file_name);
+    strcat (tex_to_pdf_cmd, " > /dev/null");
 
     FILE *input_file   = fopen (input_file_name, "r");
-    FILE *tex_file     = fopen (tex_file_name, "w");
+    FILE *tex_file     = fopen (tex_file_name, "wb");
     FILE *graphic_file = fopen (graphic_file_name, "w");
 
     if (input_file == nullptr)
@@ -63,7 +62,6 @@ int main (int argc, const char **argv)
     assert (input_file);
     assert (tex_file);
     assert (graphic_file);
-
 
     Config config = {};
 
@@ -88,9 +86,14 @@ int main (int argc, const char **argv)
 
     print_tex_ending (tex_file);
 
+    fflush (tex_file);
+    system (tex_to_pdf_cmd);
+
     fclose (tex_file);
     fclose (input_file);
     fclose (graphic_file);
+
+    free (tex_to_pdf_cmd);
 
     tree_dtor (&expr);
 }
