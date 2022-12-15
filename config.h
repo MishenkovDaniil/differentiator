2c4ab2fcb8bdd3d061be120a5dc44115e1336de2
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-12-16 02:45:22 +0300

    made more config data

--- config.h
+++ config.h
@@ -1,11 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-struct Config {
+struct Config
+{
+    char *expr = nullptr;
     double point = 0;
     int diff_order = 0;
     int decompose_order = 0;
     double decompose_point = 0;
+    double tangent_point = 0;
+    double x_min = 0;
+    double x_max = 0;
+    double y_min = 0;
+    double y_max = 0;
 };
 
 #endif /* CONFIG_H */
